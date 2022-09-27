cc3c23f6915a63a4cc96f3e665708bc004237da9
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-27 21:25:51 +0300

    psycopy and swagger installed

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third party apps:
+    'rest_framework',
+    'drf_yasg',
+    #my apps:
+
 ]
 
 MIDDLEWARE = [
