931e09d654e777395041ed5ec016359a5b59cca3
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-29 22:04:19 +0300

    reservation was added to serializer

--- flight/serializers.py
+++ flight/serializers.py
@@ -13,3 +13,40 @@ class FlightSerializer(serializers.ModelSerializer):
             'date_of_departure',
             'etd'
         )
+
+class PassengerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Passenger
+        fields= '__all__'
+
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+
+    passenger=PassengerSerializer(many=True, required=False)
+    flight=serializers.StringRelatedField()  #default =readonly
+    flight_id=serializers.IntegerField(write_only=True) #create yaparken bunu getir
+    user=serializers.StringRelatedField()  #default =readonly
+    user_id=serializers.IntegerField(write_only=True) #create yaparken bunu getir
+    class Meta:
+        model=Reservation
+        fields= ('id',
+        "flight",
+        "flight_id",
+        "user",
+        "user_id",
+        "passenger",
+        )
+
+    def create(self, validated_data):
+        passenger_data=validated_data.pop('passenger')
+        validated_data['user_id']=self.context['request'].user.id
+        reservation=Reservation.objects.create(**validated_data)
+
+        for passenger in passenger_data:
+            pas=Passenger.objects.create(**passenger)
+            reservation.passenger.add(pas)
+        reservation.save()
+        return reservation
+
+        
