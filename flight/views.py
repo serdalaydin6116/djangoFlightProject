c0d10cdc8e5efe795d1ef96e8949b0d207b6045c
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-29 22:31:52 +0300

    queryset was overriden

--- flight/views.py
+++ flight/views.py
@@ -16,5 +16,12 @@ class ReservationView(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
 
+    def get_queryset(self):
+        queryset=super().get_queryset()
+        if self.request.user.is_staff:
+            return queryset
+        return queryset.filter(user=self.request.user)
+
+
 
 
