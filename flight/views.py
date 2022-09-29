6d2c398a8c2a7747fcd060ec2484e017b5d443a5
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-29 22:05:07 +0300

    reservation view

--- flight/views.py
+++ flight/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from .serializers import FlightSerializer
+from .serializers import FlightSerializer, ReservationSerializer
 from rest_framework import viewsets
-from .models import Flight
+from .models import Flight, Passenger, Reservation
+from .permissions import IsStafforReadOnly
 
 
 class FlightView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    permission_classes= (IsStafforReadOnly,)
+
+
+
+class ReservationView(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
 
 
