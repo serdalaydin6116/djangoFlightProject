52ffe92d75e30a7a9f4bee2098ae7db7cbadf3e5
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-29 21:05:45 +0300

    flight app was created

---
+++ flight/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Flight, Passenger, Reservation
+
+
+admin.site.register(Flight)
+admin.site.register(Passenger)
+admin.site.register(Reservation)
+
+
+
+
